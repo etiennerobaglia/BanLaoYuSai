5f71254b29a122547c01513ca3d7b4936b16be66
etienne robaglia <etienne.robaglia@ext.mpsa.com>
2023-06-12 10:30:52 +0700

    vite "/" base URL

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/BanLaoYuSai",
+  base: "/",
   plugins: [vue()],
   resolve: {
     alias: {
