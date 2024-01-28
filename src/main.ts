1859085593f357eaf9b76f57e3452f343a951f46
etienne robaglia <e.robaglia@gmail.com>
2024-01-28 11:19:06 +0700

    fix router warning

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,14 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
+// import router from './router'
 
 import './assets/main.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
+// app.use(router)
 app.use(store)
 
 app.mount('#app')
